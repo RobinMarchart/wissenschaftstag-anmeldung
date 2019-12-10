5a811183e8de484d4c5b0a2463d561f031a9a9da
Robin Marchart <Robin.Marchart@hotmail.com>
2019-12-10 09:56:55 +0100

    example card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Container} from 'react-bootstrap';
+import WorkshopCard from './WorkshopCard';
 import './App.css';
+import ente from './Ente.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Container>
+      <WorkshopCard Title="TEST TITLE" Authors="ICH" Image={{src:ente,alt:'Ente'}}>
+      THIS IS AN EXAMPLE DESCRIPTION
+    </WorkshopCard>
+    <WorkshopCard Title="Workshop 2" Authors="ICH &amp; ICH" Image={{src:ente,alt:'Ente2'}}>
+      dfglhdfg sgh  hsfg hkgfdh f hgf hbfgks b sfb kjfgs bkj gf bjkfs bfgb fg bkdjsf gbjkjb   gjn sdf gdfb fgjkds bdfsgb fds gdsf gfd g bfsg fds gdfs g sdf fds g dsfh sdg hsdf g gh dfgh
+    </WorkshopCard>
+    </Container>
     </div>
+    
+    
   );
 }
 
---
+++ src/WorkshopCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Card,Media} from "react-bootstrap";
+import './WorkshopCard.css'
+
+function WorkshopCard(props){
+    return <div className="workshop-card">
+        <Card>
+    <Card.Header as="h4">
+    {props.Title}
+    </Card.Header>
+    <Card.Body>
+    <Media>
+    <img width={128} height={128} className="mr-3" src={props.Image["src"]} alt={props.Image["alt"]}/>
+    <Media.Body>
+        <div className="workshop-card-body">
+    {props.children}
+        </div>
+    </Media.Body>
+    </Media>
+    </Card.Body>
+    <Card.Footer>
+    {props.Authors}
+    </Card.Footer>
+    </Card>
+    </div>;
+}
+
+export default WorkshopCard;
