a835badeac4f0903e5a3da4d349e0a678113461c
Robin Marchart <Robin.Marchart@hotmail.com>
2019-12-12 22:08:58 +0100

    using state

--- src/WorkshopForm.jsx
+++ src/WorkshopForm.jsx
@@ -10,11 +10,8 @@ export default class WorkshopForm extends React.Component{
 
     constructor(props){
         super(props)
-        this.state={workshop:getState()};
-        this.form=new Object();
-        this.form.name1=React.createRef();
-        this.form.name2=React.createRef();
-        this.form.class=React.createRef();
+        this.state={workshop:getState(),name1:"",name2:"",class:""};
+        
         
     }
 
@@ -27,9 +24,9 @@ export default class WorkshopForm extends React.Component{
         return <Form onSubmit={this.handleSubmit.bind(this)}>
 
         <Form.Row>
-        <Form.Group controlId="formName1" as={Col} md="4">
+        <Form.Group controlId="formName1" as={Col} md="4" onChange={name1=>this.setState({name1})}>
         <Form.Label>Vorname</Form.Label>
-        <Form.Control required type="text" placeholder="Otto" ref={this.form}></Form.Control>
+        <Form.Control required type="text" placeholder="Otto"></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="formName2" as={Col} md="4">
@@ -39,7 +36,7 @@ export default class WorkshopForm extends React.Component{
 		
 		<Form.Group controlId="formClass" as={Col} md="4">
 		<Form.Label>Classe</Form.Label>
-		<Form.Control as="select" ref={this.form.class}>
+		<Form.Control as="select">
             {this.props.Classes.map((x,y)=><option key={y.toString()}>{x}</option>)}
         </Form.Control>
 		</Form.Group>
