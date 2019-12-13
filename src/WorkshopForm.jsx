684579a67d4052a1823126321b6a181287d06b0a
Robin Marchart <Robin.Marchart@hotmail.com>
2019-12-13 23:05:21 +0100

    moved to ref based input

--- src/WorkshopForm.jsx
+++ src/WorkshopForm.jsx
@@ -10,7 +10,8 @@ export default class WorkshopForm extends React.Component{
 
     constructor(props){
         super(props)
-        this.state={workshop:getState(),name1:"",name2:"",class:""};
+        this.state={workshop:getState()};
+        this.form={};
         
         
     }
@@ -24,19 +25,19 @@ export default class WorkshopForm extends React.Component{
         return <Form onSubmit={this.handleSubmit.bind(this)}>
 
         <Form.Row>
-        <Form.Group controlId="formName1" as={Col} md="4" onChange={name1=>this.setState({name1})}>
+        <Form.Group controlId="formName1" as={Col} md="4">
         <Form.Label>Vorname</Form.Label>
-        <Form.Control required type="text" placeholder="Otto"></Form.Control>
+        <Form.Control required type="text" placeholder="Otto" ref={ref=>this.form.name1=ref}></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="formName2" as={Col} md="4">
         <Form.Label>Nachname</Form.Label>
-        <Form.Control required type="text" placeholder="Maier"></Form.Control>
+        <Form.Control required type="text" placeholder="Maier" ref={ref=>this.form.name2=ref}></Form.Control>
         </Form.Group>
 		
 		<Form.Group controlId="formClass" as={Col} md="4">
 		<Form.Label>Classe</Form.Label>
-		<Form.Control as="select">
+		<Form.Control as="select" onChange={this.tmpHandle} ref={ref=>this.form.class=ref}>
             {this.props.Classes.map((x,y)=><option key={y.toString()}>{x}</option>)}
         </Form.Control>
 		</Form.Group>
