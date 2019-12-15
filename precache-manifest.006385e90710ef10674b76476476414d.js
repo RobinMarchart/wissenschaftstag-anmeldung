dc04f37e104c1684a2eac743fba2e3780669a7d7
RobinMarchart <robin.marchart@hotmail.com>
2019-12-15 13:07:23 +0000

    RobinMarchart published a site update

---
+++ precache-manifest.006385e90710ef10674b76476476414d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781e1dc7f61b515e1e6a0e5f194c15d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09dbcfa9403376c8971f",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ee0c8d7450292a50a287",
+    "url": "./static/css/main.9ddca222.chunk.css"
+  },
+  {
+    "revision": "09dbcfa9403376c8971f",
+    "url": "./static/js/2.ddb2e230.chunk.js"
+  },
+  {
+    "revision": "98e6690429c1cef066f48c1e560277ec",
+    "url": "./static/js/2.ddb2e230.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee0c8d7450292a50a287",
+    "url": "./static/js/main.cf03884d.chunk.js"
+  },
+  {
+    "revision": "b5949c292454fd049fc3",
+    "url": "./static/js/runtime-main.6368e4e8.js"
+  },
+  {
+    "revision": "a3d2b8af623b25f0e3cd71f2d77f8080",
+    "url": "./static/media/workshops.a3d2b8af.zip"
+  }
+]);
