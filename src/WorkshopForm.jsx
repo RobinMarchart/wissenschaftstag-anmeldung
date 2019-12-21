8e5eb1f244cbd97c289a0b2e00b6ff0aa3186b4a
Robin Marchart <Robin.Marchart@hotmail.com>
2019-12-21 17:02:06 +0000

    ui second workshop

--- src/WorkshopForm.jsx
+++ src/WorkshopForm.jsx
@@ -1,117 +1,130 @@
 import React from "react";
-import {Form,Col, Button} from "react-bootstrap";
-import {WorkshopCard} from "./WorkshopCard";
-import {getRegistration,setRegistration} from "./LocalWorkshopRegistrations";
+import { Form, Col, Button } from "react-bootstrap";
+import { WorkshopCard } from "./WorkshopCard";
+import { getRegistration, setRegistration } from "./LocalWorkshopRegistrations";
 import Finished from "./Finished"
 import "./WorkshopForm.css"
 
-export default class WorkshopForm extends React.Component{
+export default class WorkshopForm extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={workshop:getRegistration(),chosen:"",chosen2:"",send:null,short:false};
-        if(this.state.workshop)this.state.chosen=this.state.workshop[3];
-        this.form={};
-        this.changed=false;
+        this.state = { workshop: getRegistration(), chosen: "", chosen2: "", send: null, short: false };
+        if (this.state.workshop) {
+            this.state.chosen = this.state.workshop[3];
+            this.state.chosen2 = this.state.workshop[4];
+        }
+        this.form = {};
+        this.changed = false;
         //display warning as user leaves
-        window.addEventListener("beforeunload",this.warnIfUnsaved.bind(this));
+        window.addEventListener("beforeunload", this.warnIfUnsaved.bind(this));
     }
 
-    warnIfUnsaved(event){
-        if(this.changed&&!this.state.send){
-            let notsaved="Nicht alle änderungen wurden gesendet!";
-            let notsend="Nicht alle Änderungen wurden gesendet, bitte kurz warten!";
-            let message=(this.state.send===null)?notsaved:notsend;
+    warnIfUnsaved(event) {
+        if (this.changed && !this.state.send) {
+            let notsaved = "Nicht alle änderungen wurden gesendet!";
+            let notsend = "Nicht alle Änderungen wurden gesendet, bitte kurz warten!";
+            let message = (this.state.send === null) ? notsaved : notsend;
             (event || window.event).returnValue = message;
             return message;
         }
         else return undefined
     }
 
-    handleSubmit(x){
+    handleSubmit(x) {
         x.preventDefault();
-        let registration=[this.form.name1.value,this.form.name2.value,this.form.class.value,this.form.workshop.value,this.form.workshop2.value];
+        let registration = [this.form.name1.value, this.form.name2.value, this.form.class.value,
+        this.form.workshop.value, (this.state.short) ? this.form.workshop2.value : ""];
         console.debug(registration);
         setRegistration(registration);
-        this.setState({workshop:registration,send:false});
+        this.setState({ workshop: registration, send: false });
     }
 
-    handleWorkshopChange(x){
-        if(this.props.Workshops.find(x=>x.title===this.form.workshop.value).short)this.setState({chosen:this.form.workshop.value,send:null,short:true});
-        else this.setState({chosen:this.form.workshop.value,send:null,short:false});
-        this.changed=true;
+    handleWorkshopChange(x) {
+        if (this.props.Workshops.find(x => x.title === this.form.workshop.value).short) this.setState({ chosen: this.form.workshop.value, send: null, short: true });
+        else this.setState({ chosen: this.form.workshop.value, send: null, short: false });
+        this.changed = true;
     }
 
-    handleSecondWorkshopChange(x){
-        this.setState({chosen2:this.form.workshop2.value,send:null});
-        this.changed=true;
+    handleSecondWorkshopChange(x) {
+        this.setState({ chosen2: this.form.workshop2.value, send: null });
+        this.changed = true;
     }
 
-    renderChosenWorkshop(){
-        if(this.state.chosen){
-            let workshop=this.props.Workshops.find(x=>x.title===this.state.chosen);
-            if(workshop)return <WorkshopCard Workshop={workshop}/>
+    renderChosenWorkshop() {
+        if (this.state.chosen) {
+            let workshop = this.props.Workshops.find(x => x.title === this.state.chosen);
+            if (workshop) return <WorkshopCard Workshop={workshop} />
         }
     }
 
-    renderSecondWorkshop(){
-        if(this.state.short){
-            let workshops=this.props.Workshops
+    renderSecondWorkshop() {
+        if (this.state.short&&this.state.chosen) {
+            let workshops = this.props.Workshops.filter(x=>x.short&&(x.title!==this.state.chosen));
             return <Form.Group controlId="formWorkshop2">
-            <Form.Label>Workshop</Form.Label>
-            <Form.Control as="select" defaultValue={(this.state.workshop)?this.state.workshop[4]:null} onChange={this.handleWorkshopChange.bind(this)} ref={ref=>this.form.workshop2=ref}>
-            {workshops.map((x,y)=><option key={y.toString()}>{x.title}</option>)}
-            </Form.Control>
+                <Form.Label>Workshop</Form.Label>
+                <Form.Control as="select" defaultValue={(this.state.workshop) ? this.state.workshop[4] : null} onChange={this.handleSecondWorkshopChange.bind(this)} ref={ref => this.form.workshop2 = ref}>
+                    {workshops.map((x, y) => <option key={y.toString()}>{x.title}</option>)}
+                </Form.Control>
             </Form.Group>;
         }
     }
 
-    handleStateChange(){
-        this.setState({send:null})
-        this.changed=true;
+    renderSecondChosenWorkshop() {
+        if (this.state.short && this.state.chosen2) {
+            let workshop = this.props.Workshops.find(x => x.title === this.state.chosen2);
+            if (workshop) return <WorkshopCard Workshop={workshop} />
+        }
+    }
+
+    handleStateChange() {
+        this.setState({ send: null })
+        this.changed = true;
     }
 
 
-    render(){
-        if(this.props.ThisRef)this.props.ThisRef(this);
+    render() {
+        if (this.props.ThisRef) this.props.ThisRef(this);
         return <Form onSubmit={this.handleSubmit.bind(this)}>
 
-        <Form.Row>
-        <Form.Group controlId="formName1" as={Col} md="4">
-        <Form.Label>Vorname</Form.Label>
-        <Form.Control required type="text" onChange={this.handleStateChange.bind(this)} placeholder="Otto" defaultValue={(this.state.workshop)?this.state.workshop[0]:null} ref={ref=>this.form.name1=ref}></Form.Control>
-        </Form.Group>
+            <Form.Row>
+                <Form.Group controlId="formName1" as={Col} md="4">
+                    <Form.Label>Vorname</Form.Label>
+                    <Form.Control required type="text" onChange={this.handleStateChange.bind(this)} placeholder="Otto" defaultValue={(this.state.workshop) ? this.state.workshop[0] : null} ref={ref => this.form.name1 = ref}></Form.Control>
+                </Form.Group>
 
-        <Form.Group controlId="formName2" as={Col} md="4">
-        <Form.Label>Nachname</Form.Label>
-        <Form.Control required type="text" onChange={this.handleStateChange.bind(this)} placeholder="Maier" defaultValue={(this.state.workshop)?this.state.workshop[1]:null} ref={ref=>this.form.name2=ref}></Form.Control>
-        </Form.Group>
-		
-		<Form.Group controlId="formClass" as={Col} md="4">
-		<Form.Label>Classe</Form.Label>
-		<Form.Control as="select" onChange={this.handleStateChange.bind(this)}  defaultValue={(this.state.workshop)?this.state.workshop[2]:null} ref={ref=>this.form.class=ref}>
-            {this.props.Classes.map((x,y)=><option key={y.toString()}>{x}</option>)}
-        </Form.Control>
-		</Form.Group>
-        </Form.Row>
+                <Form.Group controlId="formName2" as={Col} md="4">
+                    <Form.Label>Nachname</Form.Label>
+                    <Form.Control required type="text" onChange={this.handleStateChange.bind(this)} placeholder="Maier" defaultValue={(this.state.workshop) ? this.state.workshop[1] : null} ref={ref => this.form.name2 = ref}></Form.Control>
+                </Form.Group>
 
-        <Form.Group controlId="formWorkshop">
-        <Form.Label>Workshop</Form.Label>
-        <Form.Control as="select" defaultValue={(this.state.workshop)?this.state.workshop[3]:null} onChange={this.handleWorkshopChange.bind(this)} ref={ref=>this.form.workshop=ref}>
-        {this.props.Workshops.map((x,y)=><option key={y.toString()}>{x.title}</option>)}
-        </Form.Control>
-        </Form.Group>
+                <Form.Group controlId="formClass" as={Col} md="4">
+                    <Form.Label>Classe</Form.Label>
+                    <Form.Control as="select" onChange={this.handleStateChange.bind(this)} defaultValue={(this.state.workshop) ? this.state.workshop[2] : null} ref={ref => this.form.class = ref}>
+                        {this.props.Classes.map((x, y) => <option key={y.toString()}>{x}</option>)}
+                    </Form.Control>
+                </Form.Group>
+            </Form.Row>
 
-        {this.renderChosenWorkshop()}
+            <Form.Group controlId="formWorkshop">
+                <Form.Label>Workshop</Form.Label>
+                <Form.Control as="select" defaultValue={(this.state.workshop) ? this.state.workshop[3] : null} onChange={this.handleWorkshopChange.bind(this)} ref={ref => this.form.workshop = ref}>
+                    {this.props.Workshops.map((x, y) => <option key={y.toString()}>{x.title}</option>)}
+                </Form.Control>
+            </Form.Group>
 
-        {this.renderSecondWorkshop()}
+            {this.renderChosenWorkshop()}
 
-        <div className="submit-group">
-        <Button type="submit">Anmelden</Button>
-        <div className="submit-status">
-        <Finished state={this.state.send}></Finished>
-        </div>
-        </div>
+            {this.renderSecondWorkshop()}
+
+            {this.renderSecondChosenWorkshop()}
+
+            <div className="submit-group">
+                <Button type="submit">Anmelden</Button>
+                <div className="submit-status">
+                    <Finished state={this.state.send}></Finished>
+                </div>
+            </div>
 
         </Form>
     }
