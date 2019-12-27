277c623eca649f12759bd9b4972e82d246e6580b
Robin Marchart <Robin.Marchart@hotmail.com>
2019-12-27 20:04:03 +0000

    fixed pgp worker

--- server/main.mjs
+++ server/main.mjs
@@ -1,14 +1,14 @@
 import fs from "fs"
 import path from "path"
 import express from "express"
-import tmp from "tmp"
+import cors from "cors"
 import crypto from "crypto"
 
 var args = process.argv.slice(2)
 
 function wrongParams() {
-	console.error("Usage: worshop-anmeldung-server port inPath outPath")
-	process.exit(1)
+    console.error("Usage: worshop-anmeldung-server port inPath outPath")
+    process.exit(1)
 }
 
 if (args.length !== 3) wrongParams();
@@ -18,23 +18,23 @@ var basePath = args[1];
 var outPath = args[2];
 
 async function run() {
-	await fs.promises.access(outPath).catch(() => fs.promises.mkdir(outPath, { recursive: true }));
-	await fs.promises.access(outPath, fs.constants.F_OK | fs.constants.W_OK)
-	var watched = new Map();
+    await fs.promises.access(outPath).catch(() => fs.promises.mkdir(outPath, { recursive: true }));
+    await fs.promises.access(outPath, fs.constants.F_OK | fs.constants.W_OK)
+    var watched = new Map();
 
-	var app = express();
-	app.put("/", function (req, res) {
-		fs.promises.writeFile(path.join(outPath, crypto.createHash("sha265").update(req.body).digest("hex")), req.body)
-			.then(() => res.sendStatus(200))
-			.catch(console.error)
-			.then(() => console.log("received registration"));
-	});
-	app.listen(port);
-	console.log("server ready")
+    var app = express();
+    app.options("/register", cors())
+    app.put("/register", cors(), function(req, res) {
+        fs.promises.writeFile(path.join(outPath, crypto.createHash("sha265").update(req.body).digest("hex")), req.body)
+            .then(() => res.sendStatus(200))
+            .catch(console.error)
+            .then(() => console.log("received registration"));
+    });
+    app.listen(port);
+    console.log("server ready")
 }
 
-run().catch(function (e) {
-	console.error(e);
-	process.exit(1);
+run().catch(function(e) {
+    console.error(e);
+    process.exit(1);
 });
-
--- src/remote.js
+++ src/remote.js
@@ -1,33 +1,33 @@
-import {encrypt,message,initWorker,key} from "openpgp";
+import { encrypt, message, initWorker, key } from "openpgp";
 import axios from "axios";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import pgpWorker from "worker-loader!./openpgp.worker.js"
 
-function log_too(data){
+function log_too(data) {
     console.log(data);
     return data;
 }
 
-async function sendRegistration(remote_config,reg_data){
-    return await axios.put(remote_config.url,log_too((await encrypt(
-        {
-        message:message.fromText(JSON.stringify(
-            {
-            data:reg_data,
-            timestamp:Math.floor(Date.now()/1000)
+async function sendRegistration(remote_config, reg_data) {
+    return await axios.put(remote_config.url + "registration", log_too((await encrypt({
+        message: message.fromText(JSON.stringify({
+            data: reg_data,
+            timestamp: Math.floor(Date.now() / 1000)
         })),
-        publicKeys:(await remote_config.key).keys,
-        armor:true
+        publicKeys: (await remote_config.key).keys,
+        armor: true
     })).data));
 }
 
-class RemoteConnection{
+class RemoteConnection {
 
-    constructor(config){
-        initWorker();
-        this.remote={url:config.url,key:key.readArmored(config.key)};
+    constructor(config) {
+        initWorker({ workers: [new pgpWorker()] });
+        this.remote = { url: config.url, key: key.readArmored(config.key) };
     }
 
-    send(reg_data){
-        return sendRegistration(this.remote,reg_data);
+    send(reg_data) {
+        return sendRegistration(this.remote, reg_data);
     }
 
 }
