955657dddfbf3f4cadd0396b96c179872149ac9c
Robin Marchart <Robin.Marchart@hotmail.com>
2020-01-23 17:17:39 +0100

    working filtering

--- auswertung/main.py
+++ auswertung/main.py
@@ -59,24 +59,46 @@ async def loadReg(path):
     decrypted=json.loads(decryptedBytes.decode("utf-8", "ignore"))
     return Registration(decrypted[2],decrypted[1],decrypted[0],decrypted[3],decrypted[4],content["timestamp"])
 
-def findClosestClasses(classes,name):
-    if name in classes:
+def findClosestClasses(classes,name,deviation=0.5):
+    if functools.reduce(lambda x,y:y or (x.Class.lower()==name.Class and x.firstName.lower()==name.firstName.lower() and x.secondName.lower()==name.secondName.lower()), classes):
         return name
-    ownClass=list(itertools.filterfalse(lambda x:x.Class!=name.Class))
-    classFirstName=[x.firstName for x in ownClass]
-    first1NameMatches=difflib.get_close_matches(name.firstName,classFirstName,n=len(classFirstName),0.6)
-    matching1FirstName=list(itertools.filterfalse(lambda y:not y in [x.firstName for x in first1NameMatches],ownClass))
-    class1SecondName=[x.secondName for x in matching1FirstName]
-    secondNameMatches1=difflib.get_close_matches(name.secondName,class1SecondName,n=len(class1SecondName),0.6)
+    ownClass=list(itertools.filterfalse(lambda x:x.Class.lower()!=name.Class.lower()))
+    classFirstName=set([x.firstName.lower() for x in ownClass])
+    classFirstNameMatches=difflib.get_close_matches(name.firstName.lower(),classFirstName,n=len(classFirstName),cutoff=deviation)
+    classMatchingFirstName=list(itertools.filterfalse(lambda x:not x.firstName.lower() in classFirstNameMatches,ownClass))
+    classSecondName=set([x.secondName.lower() for x in classMatchingFirstName])
+    classSecondNameMatches=difflib.get_close_matches(name.secondName.lower(), classSecondName,n=len(classSecondName),cutoff=deviation)
+    classMatches=list(itertools.filterfalse(lambda x:not x.secondName.lower() in classSecondNameMatches))
+    
+    FirstName=set([x.firstName.lower() for x in ownClass])
+    FirstNameMatches=difflib.get_close_matches(name.firstName.lower(),FirstName,n=len(FirstName),cutoff=deviation+0.1)
+    MatchingFirstName=list(itertools.filterfalse(lambda x:not x.firstName.lower() in FirstNameMatches,ownClass))
+    SecondName=set([x.secondName.lower() for x in MatchingFirstName])
+    SecondNameMatches=difflib.get_close_matches(name.secondName.lower(), SecondName,n=len(SecondName),cutoff=deviation+0.1)
+    matches=set(itertools.filterfalse(lambda x:not x.secondName.lower() in classSecondNameMatches)).union(set(classMatches))
 
-    FirstName=[x.firstName for x in classes]
-    first2NameMatches=difflib.get_close_matches(name.firstName,FirstName,n=len(FirstName),0.7)
-    matching2FirstName=list(itertools.filterfalse(lambda y:not y in [x.firstName for x in first2NameMatches],classes))
-    class2SecondName=[x.secondName for x in matching2FirstName]
-    secondNameMatches2=difflib.get_close_matches(name.secondName,class2SecondName,n=len(class2SecondName),0.7)
+    if len(matches)>1:
+        if(deviation<0.9):
+            return findClosestClasses(classes,name,deviation+0.1)
+        else:
+            print("to similar: {}".format(json.dumps(list(matches))))
+            return None
+    elif len(matches)==1:
+        newName=matches.pop()
+        print("replacing {} with {}".format(json.dumps(name), json.dumps(newName)))
+        return newName
+    else:
+        print("no match found for {}".format(json.dumps(name)))
+        return None
 
-    if len(secondNameMatches)<1:
-        raise Exception("multiple matchingNames")
+
+
+    #merge first names
+    #simmilar first names
+    # merge last names
+    # find matching second names
+
+    
 
 def splitExtern(regs,reg:Registration):
     if reg.Class=="extern":
@@ -85,10 +107,27 @@ def splitExtern(regs,reg:Registration):
         regs[1].append(reg)
     return regs
 
+def filter(predicate, iterable):
+    return itertools.filterfalse(lambda x:not predicate(x),iterable)
+
 async def loadRegList(path):
     files=os.listdir(path)
     paths=[pathlib.Path(path,file) for file in files]
     return [await loadReg(x) for x in paths]
+
+def externReduce(extern):
+    names=[(k,list([x[0] for x in g]))for k,g in itertools.groupby(sorted(extern))]
+    namesSorted=[(k,sorted(g)[-1])for k,g in names]
+    return namesSorted
+
+def internReduce(intern, names):
+    closest=[(reg,findClosestClasses(names,Name(Class=reg.Class,firstName=reg.firstName,secondName=reg.secondName)))for reg in intern]
+    closestFiltered=filter(lambda x:x[1]!=None,closest)
+    grouped=[(k,list([x[0] for x in g]))for k,g in itertools.groupby(sorted(closestFiltered,key=lambda x:x[1]),lambda x:x[1])]
+    sortedRegistration=[(k,list(sorted(g,key=lambda x:x.timestamp))) for k,g in grouped]
+    lastRegistrations=[(k,g[-1])for k,g in sortedRegistration]
+    return lastRegistrations
+
 
 async def main(args):
     namesPromise=loadClassList(args.classes)
@@ -97,6 +136,8 @@ async def main(args):
     regs=await regPromise
     regs_filtered=itertools.filterfalse(lambda x:x==None,regs)
     (extern,intern)=functools.reduce(splitExtern,regs_filtered,([],[]))
-
+    registrations=internReduce(intern,names)
+    registrations.extend(externReduce(extern))
+    print(registrations)
     #TODO implement splitting to workshop
 asyncio.run(main(args))
